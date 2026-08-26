24ac153dc572ab6a82024f1a56ffa552aa85f8fd
CYBER DT <darshithathikshana268@gmail.com>
2026-08-26 07:14:18 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="biopulse-v1";
+const CACHE="biopulse-v2";
 const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",e=>{
